60be728c50e660eb4f072ab44240d3de7992650b
Prasanna <prasannanatarajan3595@gmail.com>
2023-06-18 22:48:28 +0530

    Updates to include usergoal and perception

--- app.py
+++ app.py
@@ -28,11 +28,15 @@ class Mentor(Resource):
         journal = request.json.get('journal')
         goal = request.json.get('goal')
         phone_usage = request.json.get('usage')
-        print(habits,journal,goal,phone_usage)
+        usergoal = request.json.get('usergoal')
+        selfperception = request.json.get('selfperception')
+
         # Mentor Stuff
         TODAY = datetime.today()
         helping_assistant.messages = []
         helping_assistant.load_mentorship(habits,journal,phone_usage,TODAY,PAST_HISTORY)
+        if selfperception != '' and  usergoal != '':
+            helping_assistant.add_system(f'Hey this is what the user thinks of himself {selfperception} and this is his goal or ambition {usergoal}')
         completion = gpt_model.get_completion(helping_assistant,temperature=0.5,update_history=True)
 
         # Recommendation Stuff

718163414569c7a08232fcece4a3341477392198
Prasanna <prasannanatarajan3595@gmail.com>
2023-07-26 21:08:00 +0530

    api approach code

---
+++ api/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+import firebase_admin
+from firebase_admin import credentials, firestore
+import json
+from prompthandler import PromptHandler
+import os
+
+app = Flask(__name__)
+api = Api(app)
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate("./anti-distractor-firebase-adminsdk-lxfge-af9394634f.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+messages_collection = db.collection("users")
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+class Message_Completion(Resource):
+   
+    def post():
+        # Parse the JSON data from the request
+        data = request.json
+
+        # Check if API key and user id are present in the request
+        if 'apikey' not in data or 'user_id' not in data:
+            return jsonify({"error": "Invalid request"}), 400
+
+        
+        # Get the user ID from the request data
+        user_id = data['user_id']
+
+        # Query Firestore to get the message corresponding to the user ID
+        user_doc = messages_collection.document(user_id).get()
+        if not user_doc.exists:
+            return jsonify({"error": "User ID not found"}), 404
+        user_data = user_doc.to_dict()
+        # Check if the API key is valid
+        try:
+            API_KEY = user_data['apikey']
+        except KeyError:
+            return jsonify({"error": "Invalid API key"}), 401
+        if data['apikey'] != API_KEY:
+            return jsonify({"error": "Invalid API key"}), 401
+        # Get the message data from the Firestore document
+        message = user_data.get('messages',"{'head':[],'body':[]}")
+        message = json.loads(message)
+        model = PromptHandler(api_key=OPENAI_API_KEY)
+        model.load(message)
+        output = model.get_completion(data['messages'])
+        message = json.dumps(model.dump())
+        if data.get('update_history', False):
+            user_data['messages'] = message
+            messages_collection.document(user_id).set(user_data)
+        return jsonify({"response": output}), 200
+
+api.add_resource(Message_Completion, '/get_completion')
+
---
+++ api/main.py
@@ -0,0 +1,81 @@
+from flask import Flask,request
+from flask_restful import Resource, Api
+import recommendation_system
+from brain import message_history, brain_api
+from datetime import datetime,timedelta
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+
+PAST_HISTORY = 1
+openai_api_key = os.environ.get('OPENAI_API_KEY')
+
+helping_assistant = message_history()
+gpt_model = brain_api(openai_api_key)
+youtube = recommendation_system.youtube_recommender(gpt_model)
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Mentor(Resource):
+    def post(self):
+        # Retrieve data from the request
+        habits = request.json.get('habits')
+        journal = request.json.get('journal')
+        goal = request.json.get('goal')
+        phone_usage = request.json.get('usage')
+        usergoal = request.json.get('usergoal')
+        selfperception = request.json.get('selfperception')
+
+        # Mentor Stuff
+        TODAY = datetime.today()
+        helping_assistant.messages = []
+        if selfperception != '' and  usergoal != '':
+           helping_assistant.add_system(f'Hey this is what the user thinks of himself "{selfperception}" and this is his goal or ambition "{usergoal}"')
+        
+        helping_assistant.load_mentorship(habits,journal,phone_usage,TODAY,PAST_HISTORY)
+      
+        completion = gpt_model.get_completion(helping_assistant,temperature=0.5,update_history=True)
+
+        # Recommendation Stuff
+        refine_list = {}
+        helping_assistant.messages = [helping_assistant.messages[-1]] # Only loading the partner advice to save some tokens
+        refine_list['videos'] = youtube.execute(helping_assistant,goal)
+        helping_assistant.messages = []
+        refine_list['completion'] = completion[0]
+        refine_list['tokens_used'] = completion[1]
+        
+        # with open('test.json','w') as file_object:  
+        #     json.dump(refine_list,file_object)
+        return refine_list
+class Message(Resource):
+    def post(self):
+        
+        output_json = {}
+        messages = request.json.get('messages')
+        chat_model = brain_api(openai_api_key)
+        chat_msg = message_history()
+        chat_msg.messages = messages
+        model_output = chat_model.get_completion(chat_msg)
+        output_json['completion'] = model_output[0]
+        return output_json
+class Test(Resource):
+    def post(self):
+        print("Testing")
+        habits = request.json.get('habits')
+        journal = request.json.get('journal')
+        goal = request.json.get('goal')
+        phone_usage = request.json.get('usage')
+        with open('test.json','r') as file_object:  
+            data = json.load(file_object)
+        return data
+
+api.add_resource(Mentor, '/mentor')
+api.add_resource(Message, '/mentor/messages')
+api.add_resource(Test, '/test')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
